b1adf6567081098fdb6b3a4834508b0c75803a8f
Phoenix <kahlo.phoenix@gmail.com>
2017-09-23 20:08:04 -0500

    initial commit

---
+++ desktop/src/kahloringler/auxtunes/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package kahloringler.auxtunes.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import kahloringler.auxtunes.AuxTunes;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AuxTunes(), config);
+	}
+}
